5645d1a766520f116f2282490fbde50bfde45c5c
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2020-06-24 21:44:50 +0300

    p0019: optimize the solution
    So that it runs in O(N * K) instead of O(N * K * K) and takes O(1) space
    instead of O(K).
    
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

--- p0019/src/lib.rs
+++ p0019/src/lib.rs
@@ -3,20 +3,30 @@ pub fn p0019(costs: &[Vec<u32>]) -> Option<u32> {
         return None;
     }
 
-    let mut cache = [costs[0].clone(), vec![0; costs[0].len()]];
-    for i in 1..costs.len() {
-        for (k, p) in costs[i].iter().enumerate() {
-            cache[i % 2][k] = cache[(i - 1) % 2]
-                .iter()
-                .enumerate()
-                .filter(|&(ck, _)| ck != k)
-                .map(|(_, cp)| p + cp)
-                .min()
-                .unwrap()
+    let mut cost = (0, 0);
+    let mut idx = usize::MAX;
+
+    for c in costs.iter() {
+        let mut new_cost = (u32::MAX, u32::MAX);
+        let mut new_idx = 0;
+
+        for (k, p) in c.iter().enumerate() {
+            let prev_cost = if idx != k { cost.0 } else { cost.1 };
+            let cost = p + prev_cost;
+
+            if cost < new_cost.0 {
+                new_cost.0 = cost;
+                new_idx = k
+            } else if cost < new_cost.1 {
+                new_cost.1 = cost
+            }
         }
+
+        cost = new_cost;
+        idx = new_idx;
     }
 
-    cache[(costs.len() - 1) % 2].iter().min().cloned()
+    Some(cost.0)
 }
 
 #[cfg(test)]
